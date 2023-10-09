c2fb2b79ed5df5267b796976ce65cc32af23fbf8
r1d3rzz <myintthwaykhine127@gmail.com>
2023-10-09 14:47:25 +0630

    musicPlayer V1

--- script.js
+++ script.js
@@ -1,76 +1,27 @@
-const startBtn = document.querySelector(".startBtn");
-const offBtn = document.querySelector(".offBtn");
-const stopBtn = document.querySelector(".stopBtn");
-const continueBtn = document.querySelector(".continueBtn");
-const restartBtn = document.querySelector(".restartBtn");
-let stopWatchDisplay = document.querySelector(".stopWatchDisplay");
+const musicTitles = document.querySelector(".musicTitles");
+const musicPlayer = document.querySelector(".musicPlayer");
 
-let ms = 0,
-  s = 0,
-  m = 0,
-  h = 0;
-let t = 0;
-let timer;
+const tracks = [
+  { trackId: "music/track1.mp3", title: "December Nya - Lin Nat" },
+  { trackId: "music/track2.mp3", title: "Tsaw Ra Ai Tsaw Hkrup Sai - Ann Naw" },
+  {
+    trackId: "music/track3.mp3",
+    title: "Lann Mha Gyee Yey Bey - Wine Suu Khine Thein",
+  },
+  { trackId: "music/track4.mp3", title: "Yee Zarr Sar - Sai Sai Kham Hlaing " },
+];
 
-let startTimer = () => {
-  startBtn.classList.add("d-none");
-  offBtn.classList.remove("d-none");
-  stopBtn.classList.remove("disabled");
-  timer = setInterval(function () {
-    ms += 1;
-    if (ms === 99) {
-      ms = 0;
-      s += 1;
-
-      if (s === 60) {
-        s = 0;
-        m += 1;
-
-        if (m === 60) {
-          m = 0;
-          h += 1;
-        }
-      }
-    }
-    let mileSecond = ms < 10 ? 0 + ms.toString() : ms.toString();
-    let second = s < 10 ? 0 + s.toString() : s.toString();
-    let minute = m < 10 ? 0 + m.toString() : m.toString();
-    let hour = h < 10 ? 0 + h.toString() : h.toString();
-    let timeValue = `${hour}:${minute}:${second}:${mileSecond}`;
-    stopWatchDisplay.textContent = timeValue;
-  }, 10);
-};
-
-startBtn.addEventListener("click", startTimer);
-
-offBtn.addEventListener("click", function () {
-  clearInterval(timer);
-  (ms = 0), (s = 0), (m = 0), (h = 0);
-  stopWatchDisplay.textContent = "00:00:00:00";
-  startBtn.classList.remove("d-none");
-  offBtn.classList.add("d-none");
-  stopBtn.classList.add("disabled");
-  continueBtn.classList.add("d-none");
-  stopBtn.classList.remove("d-none");
-});
-
-stopBtn.addEventListener("click", function () {
-  stopBtn.classList.add("d-none");
-  continueBtn.classList.remove("d-none");
-  clearInterval(timer);
-});
-
-continueBtn.addEventListener("click", function () {
-  stopBtn.classList.remove("d-none");
-  continueBtn.classList.add("d-none");
-  clearInterval(timer);
-  startTimer();
-});
-
-restartBtn.addEventListener("click", function () {
-  clearInterval(timer);
-  (ms = 0), (s = 0), (m = 0), (h = 0);
-  continueBtn.classList.add("d-none");
-  stopBtn.classList.remove("d-none");
-  startTimer();
+let i = 0;
+tracks.forEach((track) => {
+  i += 1;
+  const music = document.createElement("div");
+  music.setAttribute("id", i);
+  music.classList.add("mb-3", "musicNameBtn");
+  music.addEventListener("click", () => {
+    musicPlayer.src = track.trackId;
+    musicPlayer.play();
+  });
+  const musicName = `${i}. ${track.title}`;
+  music.append(musicName);
+  musicTitles.append(music);
 });
